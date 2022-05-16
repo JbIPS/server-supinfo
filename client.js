775e98d0e140a22144730cf1b964280a028cea51
JbIPS <jeanbaptiste.richardet@gmail.com>
2022-05-16 15:40:52 +0200

    Create basic HTTP server

---
+++ client.js
@@ -0,0 +1,15 @@
+const axios = require('axios').default;
+
+async function start() {
+        const result = await axios.get('http://localhost:3000/products');
+        console.log(result.data);
+
+        const putResult = await axios.put('http://localhost:3000/products/2', {
+                name: 'Règle',
+                price: 2
+        });
+        console.log(putResult.status, putResult.statusText, putResult.data);
+}
+
+start();
+
