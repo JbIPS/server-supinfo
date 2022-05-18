a01b335448c6911d980ade05817dc17bcfd08e4d
emeric boyadjian <emericboy73@gmail.com>
2022-05-18 10:18:21 +0200

    change indent client.js

--- client.js
+++ client.js
@@ -1,15 +1,14 @@
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 async function start() {
-        const result = await axios.get('http://localhost:3000/products');
-        console.log(result.data);
+  const result = await axios.get("http://localhost:3000/products");
+  console.log(result.data);
 
-        const putResult = await axios.put('http://localhost:3000/products/2', {
-                name: 'Règle',
-                price: 2
-        });
-        console.log(putResult.status, putResult.statusText, putResult.data);
+  const putResult = await axios.put("http://localhost:3000/products/2", {
+    name: "Règle",
+    price: 2,
+  });
+  console.log(putResult.status, putResult.statusText, putResult.data);
 }
 
 start();
-
