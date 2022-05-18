b56c3f575c0daada57a58b828c23a8ef5ec155a8
emeric boyadjian <emericboy73@gmail.com>
2022-05-18 10:17:31 +0200

    change indent index.js

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-const fastify = require('fastify')({ logger: true })
+const fastify = require("fastify")({ logger: true });
 
-const items = [{
-        id: 1,
-        name: 'Cartable',
-        price: 4
-}]
+const items = [
+  {
+    id: 1,
+    name: "Cartable",
+    price: 4,
+  },
+];
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-        const qs = request.query;
-        if (qs.from === 'jb') {
-                return `Hello ${qs.from}: here's your secret!`;
-        } else {
-                reply.code(401).send('Unauthorized')
-        }
-})
-
-fastify.get('/products', async (request, reply) => {
-        return items;
-})
-
-fastify.get('/products/:id', async (request, reply) => {
-        const id = parseInt(request.params.id, 10);
-        const desiredItem = items.find((item) => item.id === id);
-        if (desiredItem) {
-                return desiredItem;
-        } else {
-                reply.code(404).send('not found');
-        }
+fastify.get("/", async (request, reply) => {
+  const qs = request.query;
+  if (qs.from === "jb") {
+    return `Hello ${qs.from}: here's your secret!`;
+  } else {
+    reply.code(401).send("Unauthorized");
+  }
 });
 
-fastify.put('/products/:id', async (request, reply) => {
-        const id = parseInt(request.params.id, 10);
-        if (!request.body.name || !request.body.price) {
-                reply.code(400).send('Product must have a name and a price')
-        } else {
-                items.push({
-                        id: id,
-                        name: request.body.name,
-                        price: request.body.price
-                });
-                return reply.code(201).send(`Product ${request.body.name} has been created`);
-        }
-})
+fastify.get("/products", async (request, reply) => {
+  return items;
+});
+
+fastify.get("/products/:id", async (request, reply) => {
+  const id = parseInt(request.params.id, 10);
+  const desiredItem = items.find((item) => item.id === id);
+  if (desiredItem) {
+    return desiredItem;
+  } else {
+    reply.code(404).send("not found");
+  }
+});
+
+fastify.put("/products/:id", async (request, reply) => {
+  const id = parseInt(request.params.id, 10);
+  if (!request.body.name || !request.body.price) {
+    reply.code(400).send("Product must have a name and a price");
+  } else {
+    items.push({
+      id: id,
+      name: request.body.name,
+      price: request.body.price,
+    });
+    return reply
+      .code(201)
+      .send(`Product ${request.body.name} has been created`);
+  }
+});
 
 // Run the server!
 const start = async () => {
-        try {
-                await fastify.listen(3000, '0.0.0.0')
-        } catch (err) {
-                fastify.log.error(err)
-                process.exit(1)
-        }
-}
-start()
+  try {
+    await fastify.listen(3000, "0.0.0.0");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
