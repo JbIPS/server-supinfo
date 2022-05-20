e88c0b83a5a2d94f310cc6bad68161265cfc375d
Gresse Baptiste <Baptiste.gresse@epitech.digital>
2022-05-20 13:00:45 +0200

    final version

--- client.js
+++ client.js
@@ -8,7 +8,24 @@ async function start() {
                 name: 'Cartable',
                 price: 2
         });
-        console.log(putResult.status, putResult.statusText, putResult.data);
+
+        const pushResult = await axios.post("http://localhost:3000/products/new-product", {
+                name: "Chaise",
+                price: 4,
+          });
+
+        const patchResult = await axios.patch("http://localhost:3000/products/2", {
+	        newPrice: 9
+	});
+
+        try {
+		const deleteResult = await axios.delete("http://localhost:3000/products/2", { headers: {
+			authorization: "Baptiste"
+		} });
+		console.log(deleteResult.status, deleteResult.statusText, deleteResult.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 start();
--- index.js
+++ index.js
@@ -1,56 +1,147 @@
-const fastify = require('fastify')({ logger: true })
+const fastify = require("fastify")({ logger: true });
+const rateLimit = require('@fastify/rate-limit');
+const helmet = require('@fastify/helmet');
+const authPlugin = require("./authPlugin");
 
-const items = [{
-        id: 1,
-        name: 'Cartable',
-        price: 4
-}]
+const items = [
+  {
+    id: 1,
+    name: "Cartable",
+    price: 4,
+  },
+];
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-        const qs = request.query;
-        if (qs.from === 'jb') {
-                return `Hello ${qs.from}: here's your secret!`;
-        } else {
-                reply.code(401).send('Unauthorized')
-        }
-})
+const users = [
+	{
+		mail: 'enzo',
+		password: 'MotdePasse#'
+	},
+	{
+		mail: 'rick',
+		password: 'MotdePasse#'
+	}
+];
 
-fastify.get('/products', async (request, reply) => {
-        return items;
-})
-
-fastify.get('/products/:id', async (request, reply) => {
-        const id = parseInt(request.params.id, 10);
-        const desiredItem = items.find((item) => item.id === id);
-        if (desiredItem) {
-                return desiredItem;
-        } else {
-                reply.code(404).send('not found');
-        }
+fastify.register(authPlugin, {
+	users: users
 });
 
-fastify.put('/products/:id', async (request, reply) => {
-        const id = parseInt(request.params.id, 10);
-        if (!request.body.name || !request.body.price) {
-                reply.code(400).send('Product must have a name and a price')
-        } else {
-                items.push({
-                        id: id,
-                        name: request.body.name,
-                        price: request.body.price
-                });
-                return reply.code(201).send(`Product ${request.body.name} has been created`);
-        }
-})
+fastify.register(rateLimit, {
+	max: 7,
+	timeWindow: '30 secondes'
+});
+
+fastify.register(helmet,{ 
+	contentSecurityPolicy: false 
+});
+
+// Declare a route
+fastify.get("/", async (request, reply) => {
+  const qs = request.query;
+	return `hello ${request.user}`;
+});
+
+fastify.get("/products", async (request, reply) => {
+	return items;
+});
+
+fastify.get("/products/:id", async (request, reply) => {
+  const id = parseInt(request.params.id, 10);
+  const desiredItem = items.find((item) => item.id === id);
+  if (desiredItem) {
+    return desiredItem;
+  } else {
+    reply.code(404).send("not found");
+  }
+});
+
+fastify.put("/products/:id", async (request, reply) => {
+  const id = parseInt(request.params.id, 10)
+
+  if (!request.body.name || !request.body.price) {
+    reply.code(400).send("Product must have a name and a price");
+  } else {
+		const existingPorductKey = items.findIndex((item) => item.id === id);
+
+    if(existingPorductKey != -1){
+      items[existingPorductKey] = {
+				id: id,
+				name: request.body.name,
+				price: request.body.price,
+			}
+			return reply
+				.code(201)
+				.send(`Product ${request.body.name} has been updated`);
+    }else {
+			items.push({
+				id: id,
+				name: request.body.name,
+				price: request.body.price,
+			});
+			return reply
+				.code(201)
+				.send(`Product ${request.body.name} has been created`);
+    }
+  }
+});
+
+fastify.post("/products/new-product", async (request, reply) => {
+	const lastItem = items[items.length - 1];
+  const id = lastItem.id + 1;
+
+  if (!request.body.name || !request.body.price) {
+    reply.code(400).send("Product must have a name and a price");
+  } else {
+		items.push({
+			id: id,
+			name: request.body.name,
+			price: request.body.price,
+		});
+		return reply
+			.code(201)
+			.send(`Product ${request.body.name} has been created`);
+  }
+});
+
+fastify.patch("/products/:id", async (request, reply) => {
+	const id = parseInt(request.params.id, 10);
+	const price = parseInt(request.body.newPrice, 10);
+	const existingPorductKey = items.findIndex((item) => item.id === id);
+
+	if(existingPorductKey != -1){
+		items[existingPorductKey].price = price;
+		return reply
+			.code(200)
+			.send(`Product ${id} has been updated`);
+	}else {
+		return reply
+		.code(404)
+			.send(`Error: no product found`);
+	}
+});
+
+fastify.delete("/products/:id", async (request, reply) => {
+	const id = parseInt(request.params.id, 10);
+	const itemToRemoveKey = items.findIndex((item) => item.id === id);
+
+	if(itemToRemoveKey != -1){
+		items.splice(itemToRemoveKey, 1);
+		return reply
+			.code(204);
+	}else {
+		return reply
+		.code(404)
+			.send(`Error: no product found`);
+	}
+});
 
 // Run the server!
 const start = async () => {
-        try {
-                await fastify.listen(3000, '0.0.0.0')
-        } catch (err) {
-                fastify.log.error(err)
-                process.exit(1)
-        }
-}
-start()
+  try {
+    await fastify.listen(3000, "0.0.0.0");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
